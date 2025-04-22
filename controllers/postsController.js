eb01e456caa4d28ab82c39b622d54b4ae51358bc
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-04-22 16:30:08 +0200

    [UPD] creazione dei metodi nel file controller

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -5,3 +5,31 @@ const posts = require('../data/posts.js');
 function index (req, res){
     res.json(posts);
 }
+
+// definizionedella funzione show
+function show (req, res){
+    res.send('posts con id' + ' ' + req.params.id);
+}
+
+// definizionedella funzione store
+function store (req, res){
+    res.send('Inserimento di un nuovo post');
+}
+
+// definizione della funzione update
+function update (req, res){
+    res.send('Modifica totale del post' + ' ' + req.params.id);
+}
+
+// definizione della funzione modify
+function modify(re, res){
+    res.send('Modifica parziale di un post' + ' ' + req.params.id);
+}
+
+// definizione della funzione destroy
+function destroy(req,res){
+    res.send(`Cancellazione di un post' ${req.params.id}`);
+}
+
+// esporto il controller
+module.exports = {index, show, store, update, modify, destroy}
