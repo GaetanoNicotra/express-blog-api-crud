4facb824eebcf61d26aa3cb7e7415c34d8138f0c
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-04-23 15:24:39 +0200

    [UPD] milestone 2 completata

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ const port = 3000;
 // importo le rotte
 const postsRouter = require('./router/posts.js');
 
+// uso il body parser
+app.use(express.json());
+
 // utilizzo il router
 app.use('/posts', postsRouter);
 
-// uso il body parser
-app.use(express.json());
+
 
 // definisco la rotta base
 app.get('/', (req, res) => {
